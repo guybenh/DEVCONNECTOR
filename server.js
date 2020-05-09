1b0c9448bf0b8d72bf8c44307e85b128a5160458
guywix <guyha@wix.com>
2020-05-09 15:39:19 +0300

    connect to monogoDB

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+//connect database
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
