db3be9eb5a3e8901d5ebb7b00efa18ba3069bf86
guywix <guyha@wix.com>
2020-09-28 18:54:10 +0300

    started frontend

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/layout/NavBar';
+import Landing from "./components/layout/Landing";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+
+const App = () => (
+  <Router>
+    <div>
+      <NavBar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </div>
+  </Router>
+);
+
+export default App;
